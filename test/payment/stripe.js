a320ef751912c8e1a7a714ed37de6f3423fa6f2e
KKudryavtsev <konstantin.k.ed@gmail.com>
2014-10-08 10:37:21 +0100

    quote fee tests

--- test/payment/stripe.js
+++ test/payment/stripe.js
@@ -4,7 +4,7 @@ var exec = require("child_process").exec,
 module.exports = function(testApp, opt){
   describe("with stripe", function(){
     describe("using a card", function(){
-      var cardToken, paymentProvider,
+      var cardToken, paymentProvider,card,
           usageType = "personal";
       
       beforeEach(function(done){
@@ -38,34 +38,32 @@ module.exports = function(testApp, opt){
               });
             }).catch(function(err){ console.trace(err); }).then(function(res){
               cardToken = res.body["card-tokens"][0];
+              return testApp.request.get("/users/"+opt.bob.id+"/cards");
+            }).then(function(data){
+              var cards = data.body.cards;
+              cards.length.should.be.equal(1);
+              
+              card = cards[0];
               done();
-            });              
+            });
           }
         });
       });
       
       describe("once registered", function(){
-        it("creates a card resource", function(done){
-          return testApp.request.get("/users/"+opt.bob.id+"/cards").then(function(data){
-            var cards = JSON.parse(data.body).cards,
-                card = cards[0];
-
-            cards.length.should.be.equal(1);
-            card.links.user.should.be.equal(opt.bob.id);
-            card.links.cardTokens.length.should.be.equal(1);
-            card.links.cardTokens[0].should.be.equal(cardToken.id);
-            card.links.paymentProvider.should.be.equal(paymentProvider.id);
-            card.usageType.should.be.equal(usageType);
-            card.last4.should.be.equal("4242");
-            card.expiresAt.should.be.eql({
-              date: "2015-03-01",
-              time: "00:00",
-              timeZone: 0
-            });
-            card.name.should.be.equal("Bob Bob");
-
-            done();
-          }).catch(function(err){ console.trace(err); });
+        it("creates a card resource", function(){
+          card.links.user.should.be.equal(opt.bob.id);
+          card.links.cardTokens.length.should.be.equal(1);
+          card.links.cardTokens[0].should.be.equal(cardToken.id);
+          card.links.paymentProvider.should.be.equal(paymentProvider.id);
+          card.usageType.should.be.equal(usageType);
+          card.last4.should.be.equal("4242");
+          card.expiresAt.should.be.eql({
+            date: "2015-03-01",
+            time: "00:00",
+            timeZone: 0
+          });
+          card.name.should.be.equal("Bob Bob");
         });
 
         it("stores stripe customer id on the user", function(done){
@@ -77,17 +75,39 @@ module.exports = function(testApp, opt){
       });
 
       describe("once paid", function(){
+        var quote, charge;
+        
         beforeEach(function(done){
-          testApp.fixture.create("quote").then(function(){
-            //   return testApp.trustedFortuneClient.getQuotes();
-            // }).then(function(data){
-            //   console.log(data.quotes);
-            
+          testApp.fixture.create("quote", {
+            user: opt.bob.id
+          }).then(function(data){
+            quote = data.quotes[0];
+
+            return testApp.request.post({
+              url: "/charges",
+              json: {
+                charges: [{
+                  card: card.id,
+                  quote: quote.id
+                }]
+              }
+            });
+          }).then(function(){
+            return testApp.request.get("/cards/"+card.id+"/charges");
+          }).then(function(res){
+            var charges = res.body.charges;
+            charge = charges[0];
             done();
           });
         });
         
         it("creates a charge", function(done){
+          console.log("charge:",charge);
+          charge.quote.should.be.equal(quote.id);
+          charge.amount.amount.should.be.equal(quote.price.amount);//mb not
+          charge.amount.currency.should.be.equal(quote.price.currency);//mb not :)
+          charge.refunded.should.be.equal(false);
+          charge.paid.should.be.equal(true);
           done();
         });
       });
--- test/quote/index.js
+++ test/quote/index.js
@@ -1,3 +1,5 @@
+var _ = require("lodash");
+
 module.exports = function(testApp){
   describe("quote", function(){
     var quote,
@@ -7,11 +9,11 @@ module.exports = function(testApp){
       testApp.fixture.registerAndSignIn().then(function(user){
         opts.user = user;
 
-        return testApp.fixture.create("quote");
+        return testApp.fixture.create("quote",{user: user.id});
       }).then(function(data){
         quote = data.quotes[0];
         done();
-      });
+      }).catch(function(err){ console.trace(err); });
     });
 
     it("uses the conventional currency format", function(done){
@@ -28,5 +30,80 @@ module.exports = function(testApp){
         done();
       });
     });
+
+    describe("payment fees", function(){
+      var paymentFees, amexFee, visaFee;
+      
+      var skrillFeePercent = 20,
+          stripeFeePercent = 10;
+
+      beforeEach(function(done){
+        
+        testApp.fixture.create("payment-provider",[{
+          name: "skrill"
+        },{
+          name: "stripe"
+        }]).then(function(){
+          return testApp.fixture.create("payment-type", [{
+            name: "visa",
+            paymentProvider: "stripe",
+            feePercent: stripeFeePercent
+          }, {
+            name: "amex",
+            paymentProvider: "skrill",
+            feePercent: skrillFeePercent
+          }]);
+        }).then(function(){
+          //TODO: nest this under "when requested for a quote"
+
+          return testApp.request.get({
+            url: "/quotes/"+quote.id,
+            qs: {
+              include: "paymentFees,user"
+            }
+          });
+        }).then(function(res){
+          var paymentFees = res.body.linked["payment-fees"];
+
+          paymentFees.length.should.be.equal(2);
+
+          var feeForType = function(type){
+            return _.find(paymentFees, function(fee){
+              return fee.links.paymentType === type;
+            });
+          };
+
+          amexFee = feeForType("amex"),
+          visaFee = feeForType("visa");
+
+          done();
+        });
+      });
+      
+      it("link to a quote", function(done){
+        amexFee.links.quote.should.be.equal(quote.id);
+        visaFee.links.quote.should.be.equal(quote.id);
+        
+        done();
+      });
+
+      it("are calculated correctly", function(){
+        var skrillProviderFee = quote.price.amount * (skrillFeePercent/100),
+            skrillTotal = quote.price.amount + skrillProviderFee,
+            stripeProviderFee = quote.price.amount * (stripeFeePercent/100),
+            stripeTotal = quote.price.amount + stripeProviderFee;
+        
+        amexFee.amount.amount.should.be.equal(quote.price.amount);
+        amexFee.amount.currency.should.be.equal(quote.price.currency);
+        amexFee.providerFee.amount.should.be.equal(skrillProviderFee);
+        amexFee.total.amount.should.be.equal(skrillTotal);
+
+        visaFee.amount.amount.should.be.equal(quote.price.amount);
+        visaFee.amount.currency.should.be.equal(quote.price.currency);
+        visaFee.providerFee.amount.should.be.equal(stripeProviderFee);
+        visaFee.total.amount.should.be.equal(stripeTotal);
+      });
+    });
+
   });
 };
